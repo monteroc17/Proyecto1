f6e2ea32a9ef264d9f28dde9761ccf369f511f3c
heisenbergCR <d.montero95@gmail.com>
2014-09-13 18:18:33 -0600

    Proyecto.2
    Esta versión está casi terminada. Le falta el detalle de los espacios de
    más y la validación de errores

--- Projecto1/Interprete.py
+++ Projecto1/Interprete.py
@@ -9,19 +9,35 @@ def crear():#Crear un nuevo archivo de python
     return archivo#se retorna para que el archivo sea utilizado en la función compilar
 
 def compilar(archivo):#recibe como parametro el archivo .py crreado anteriormnte
+    print("Digite su codigo aqui")
+    print("la palabra terminar de escribir codigo es 'compilar'")
     #lista con las palabras reservadas de python
-    comandos_originales=["  ","\n","(",")","'","if","else","elif","and","def","del","is","from","not","or","try","in","as","for","return","break","while","import","print","input","int","str","float"]
-
+    comandos0={"":"_","(":"♪",")":"♫","'":";","if":"se","else":"altro","elif":"setro","and":"con","def":"definire","del":"cance","is":"un","from":"da","not":"non","or":"oppure","try":"provare","in":"dentro","as":"come","for":"per","return":"ritorno","break":"pausa","while":"mentre","import":"importare","print(":"stampa(","input(":"ingresso(","int(":"intero(","int":"intero","str(":"stri(","str":"stri","float(":"flultuare(","float":"flultuare"}
+    comandos_originales=["():","()",":","   ","(",")","'","if","else","elif","and","def","del","is","from","not","or","try","in","as","for","return","break","while","import","print(","print('","input(","input('","int(","int('","int","str(","str('","str","float(","float('","float","')","'))","')))"]
     #lista con las palabras del nuevo codigo
-    comandos_nuevos=["►",";","♪","♫","☼","se","altro","setro","con","definire","cance","un","da","non","oppure","provare","dentro","come","per","ritorno","pausa","mentre","importare","stampa","ingresso","intero","stri","flultuare"]
+    comandos_nuevos=["♪♫^","♪♫","^","_","♪","♫",";","se","altro","setro","con","definire","cance","un","da","non","oppure","provare","dentro","come","per","ritorno","pausa","mentre","importare","stampa♪","stampa♪;","ingresso♪","ingresso♪;","intero♪","intero♪;","intero","stri♪","stri♪;","stri","flultuare♪","flultuare♪;","flutuare",";♫",";♫♫",";♫♫♫"]
+
+    comandos=dict(zip(comandos_nuevos,comandos_originales))#convierte las 2 listas en un diccionario
     contador=0
+
     while contador==0:
-        codigo=str(input("Digite aquí su codigo:"))
+        codigo=str(input(">>"))
         if codigo=="compilar":
             contador=1
             print("Su codigo se ha guardado exitosamente!")
         else:
-            archivo.write(codigo+"\n")
+            x=codigo.split()
+            for i in x:
+                lista=[]
+                if i in comandos:
+                    lista.append(comandos[i])
+                else:
+                    lista.append(i)
+                codigo_final=" ".join(lista)
+                archivo.write(codigo_final+" ")
+            archivo.write("\n")
+
+
 
 
     print("en proceso")
@@ -45,6 +61,7 @@ def menu():
     if opcion==1:
         crear()#Aquí se llama a la funcion "crear"
     elif opcion==2:
-        compilar()#Aquí se llama a la funcion "compilar"
+        print("No existe ningún archivo para compilar!\nPor Favor, cree un archivo primero")
+        menu()
 
 menu()
